045483c59dfbd2ec484676682f25a464cac34bab
LillaDou <lilla.e.j.du@gmail.com>
2024-07-22 19:08:56 +0200

    Argumentos Rest en funciones

---
+++ funciones/args-rest.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    //! Operador Rest: son los ... que hemos puesto como argumentos en la función, mas el nombre que le ponemos. 
+    // Es originalmente de type any[], por lo que debemos indicar el tipo que queremos que sea. 
+    // En este ejemplo nos indica que el firstName es obligatorio siempre, pero el resto de 
+    // argumentos que añadamos (Joseph, Kent...) serán flexibles. Es decir, si cambian o se añaden más nombres
+    // no pasa nada, se añadirán en consola por el .join(' '). 
+    const fullName = (firstName, ...restArgs) => {
+        return `${firstName} ${restArgs.join(' ')}`;
+    };
+    const superman = fullName('Clark', 'Joseph', 'Kent');
+    console.log({ superman });
+})();
