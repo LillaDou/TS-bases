dd29f74ccc6ac74c3bb44005b80c3eacb0a9bcef
LillaDou <lilla.e.j.du@gmail.com>
2024-07-22 11:10:43 +0200

    Void

---
+++ tipos/void.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    //Void: vacío, no hay nada. 
+    //
+    function callBatman() {
+        return;
+    }
+    const callSuperman = () => {
+    };
+    const a = callBatman();
+    console.log(a);
+})();
---
+++ tipos/void.ts
@@ -0,0 +1,18 @@
+
+(() => {
+
+    //Void: vacío, no hay nada. 
+    //
+
+    function callBatman(): void {
+        return;
+    }
+
+    const callSuperman = (): void => {
+
+    }
+
+    const a = callBatman();
+    console.log(a);
+
+})()
