c3827b23336ded13b4aeb986dc8887df5b93bb24
LillaDou <lilla.e.j.du@gmail.com>
2024-07-22 10:27:55 +0200

    Arrays

---
+++ tipos/arrays.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    // const numbers: (string | number | boolean)[] = [1, 2, 3, 4, 5, '6', 7, 8, 9, 10]; //arreglo de numbers o string, o boolean
+    // numbers.push('true');//Acepta tanto un string como un number, y un booleano si lo indicamos en el const
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const villains = ['Omega Rojo', 'Dormammu', 'Duende Verde'];
+    villains.forEach(v => console.log(v.toUpperCase));
+})();
