89526b3bbb401cfbf0a5b99651a9ac24a06efc8c
LillaDou <lilla.e.j.du@gmail.com>
2024-07-22 11:17:57 +0200

    Never

---
+++ tipos/never.ts
@@ -0,0 +1,19 @@
+
+(() => {
+
+    //El never significa que mi función no debe de terminar excitosamente
+    //No tiene que tener un punto alcanzable al realizarse
+
+    const error = (message: string): (never | number) => {
+        if( false ) {
+            throw new Error(message);
+        }
+
+        return 1;
+    }
+    //O da error, y por lo tanto es un never, o regresa un número. 
+
+    error('Auxilio');//Tras esto, el código no se sigue ejecutando. 
+    console.log('Hola Mundo');//Ya no se ejecuta por la función de error
+
+})()
