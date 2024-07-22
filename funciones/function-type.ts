f1174613d0b8f6d7d4842518554db967217e67e5
LillaDou <lilla.e.j.du@gmail.com>
2024-07-22 19:25:24 +0200

    Tipo de funciones y cómo nombrarlas/definirlas

---
+++ funciones/function-type.ts
@@ -0,0 +1,27 @@
+(() => {
+
+    const addNumbers = ( a: number, b: number ) => a + b;
+    const greet = ( name: string ) => `Hola ${ name }`;
+    const saveTheWorld = ( ) => `El mundo está salvado!`;
+
+    let myFunction; //any!
+    //Definimos el myFunction para que no sea de tipo any de las siguientes maneras:
+
+    
+    //let myFunction: Function -- este da error porque no es una función.
+    //* myFunction = 10;
+    //* console.log(myFunction);
+
+    //let myFunction: (y: number, z: number) => number; Es una función con estructura como la de addNumber
+    //? myFunction = addNumbers; //mandamos la referencia, no la ejecutamos, por lo que no ponemos los ()
+    //? console.log( myFunction( 1, 2 ) );
+
+    //let myFunction: (y: string) => string; Función como la de greet
+    //* myFunction = greet;
+    //* console.log( myFunction( 'Fernando' ) );
+
+    //let myFunction: () => string; Función como la de saveTheWorld
+    //? myFunction = saveTheWorld;
+    //? console.log( myFunction( ) );
+
+})();
