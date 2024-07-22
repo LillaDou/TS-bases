62342e2cdf41ded836e9ff8d72c63b0c2a23cb4c
LillaDou <lilla.e.j.du@gmail.com>
2024-07-22 11:22:36 +0200

    Null-undefined

---
+++ tipos/null-undefined.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    //strictNullChecks a false en el tsconfig
+    //!Null no es lo mismo que undefined
+    // let isActive: (boolean|undefined) = undefined;
+    // console.log(isActive);
+})();
---
+++ tipos/null-undefined.ts
@@ -0,0 +1,12 @@
+(() => {
+
+
+    //strictNullChecks a false en el tsconfig
+
+    //!Null no es lo mismo que undefined
+    
+    // let isActive: (boolean|undefined) = undefined;
+
+    // console.log(isActive);
+
+})();
