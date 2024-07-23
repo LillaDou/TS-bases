030edf6b0185065139f92ee603cc1b97371b75fd
LillaDou <lilla.e.j.du@gmail.com>
2024-07-23 16:21:09 +0200

    outFile - archivo de salida

--- app-funciones.ts
+++ app-funciones.ts
@@ -1,35 +1,41 @@
-// Funciones Básicas
-function sumar( a: number, b: number ): number{
-  return a + b;
-}
+(() => {
 
-const contar = ( heroes: string[] ): number => {
-  return heroes.length;
-}
-const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
-contar(superHeroes);
-
-//Parametros por defecto
-const llamarBatman = ( llamar: boolean = false ): void => {
-  if( llamar ){
-    console.log("Batiseñal activada");
+    // Funciones Básicas
+  function sumar( a: number, b: number ): number{
+    return a + b;
   }
-}
-//Si no regresa nada la función, es de tipo void
 
-llamarBatman();
+  const contar = ( heroes: string[] ): number => {
+    return heroes.length;
+  }
+  const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+  contar(superHeroes);
+
+  //Parametros por defecto
+  const llamarBatman = ( llamar: boolean = false ): void => {
+    if( llamar ){
+      console.log("Batiseñal activada");
+    }
+  }
+  //Si no regresa nada la función, es de tipo void
+
+  llamarBatman();
 
 
 
-// Rest?
-const unirheroes = ( ...personas: string[] ): string => {
-  return personas.join(", ");
-}
+  // Rest?
+  const unirheroes = ( ...personas: string[] ): string => {
+    return personas.join(", ");
+  }
 
 
-// Tipo funcion
-const noHaceNada = ( numero: number, texto: string, booleano: boolean, arreglo: string[] )=> {}
+  // Tipo funcion
+  const noHaceNada = ( numero: number, texto: string, booleano: boolean, arreglo: string[] )=> {}
 
-// Crear el tipo de funcion que acepte la funcion "noHaceNada"
-let noHaceNadaTampoco: (n: number, t: string, b: boolean, a: string[]) => void;
-noHaceNadaTampoco = noHaceNada;
+  // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+  let noHaceNadaTampoco: (n: number, t: string, b: boolean, a: string[]) => void;
+  noHaceNadaTampoco = noHaceNada;
+
+})()
+
+
--- funciones/function-type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(() => {
-    const addNumbers = (a, b) => a + b;
-    const greet = (name) => `Hola ${name}`;
-    const saveTheWorld = () => `El mundo está salvado!`;
-    let myFunction;
-})();
-//# sourceMappingURL=function-type.js.map
