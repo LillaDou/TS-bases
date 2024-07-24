fd2fe9b12c5d4f43524fe8dc709b4301df17d173
LillaDou <lilla.e.j.du@gmail.com>
2024-07-24 17:13:55 +0200

    Forma corta de asignar propiedades en las clases de TS

--- classes/basica.ts
+++ classes/basica.ts
@@ -3,20 +3,22 @@
 
     class Avenger {
 
-        private name: string;
-        private team: string;
-        public realName?: string;
-        static avgAge: number = 35; 
+        // private name: string;
+        // private team: string;
+        // public realName?: string;
+        static avgAge: number = 35; //no hace falta incluirla en el constructor
 
-        constructor( name: string, team: string, realName?: string) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        }
+        //Esta es una forma muy común de hacer el constructor. Ahorramos tener que escribirlos
+        //de la manera que indicamos originalmente (arriba)
+        constructor( 
+            private name: string, 
+            private team: string, 
+            public realName?: string
+        ) { }
         //Con el constructor podemos acceder a las propiedades que son private y public
     }
 
-    const antman: Avenger = new Avenger('Antman', 'Capitan');
+    const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
     // console.log(Avenger.avgAge)//Se puede acceder a los static de forma global, mencionando a la 
     //clase (Avenger) y a la propiedad (avgAge)
