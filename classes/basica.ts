e0c08373555a506603208063dadb00f4e8e62402
LillaDou <lilla.e.j.du@gmail.com>
2024-07-25 16:24:27 +0200

    Métodos públicos y privados

--- classes/basica.ts
+++ classes/basica.ts
@@ -1,3 +1,7 @@
+//!Static: poder acceder a él sin crear instancias de la clase. 
+//Solo se hace referencia a la clase. E.g. Avenger.getAvgAge(), en vez de ironman.getAvgAge()
+//!Público: puedes acceder a él fuera de la clase
+//!Private: solo puedes hacer cambios dentro de la clase
 
 (() => {
 
@@ -7,6 +11,9 @@
         // private team: string;
         // public realName?: string;
         static avgAge: number = 35; //no hace falta incluirla en el constructor
+        static getAvgAge() {
+            return this.name; //OJO! nombre de la clase!! 
+        }
 
         //Esta es una forma muy común de hacer el constructor. Ahorramos tener que escribirlos
         //de la manera que indicamos originalmente (arriba)
@@ -16,11 +23,19 @@
             public realName?: string
         ) { }
         //Con el constructor podemos acceder a las propiedades que son private y public
+    
+        
+        public bio() {
+            return `${ this.name } (${ this.team })`;
+        }
     }
+
 
     const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
     // console.log(Avenger.avgAge)//Se puede acceder a los static de forma global, mencionando a la 
     //clase (Avenger) y a la propiedad (avgAge)
+
+    console.log( antman.bio() );
 
 })()
--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 "use strict";
 (() => {
     class Avenger {
+        static getAvgAge() {
+            return this.name;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
+    console.log(antman.bio());
 })();
 //# sourceMappingURL=main.js.map
